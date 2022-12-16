8734ba97b887432acb105cea319551c2fbe72aef
Nilay Nagar <45895008+nilzzzz19@users.noreply.github.com>
2022-12-16 15:05:25 -0800

    search works

--- GeoTags/app/src/main/java/android/google/maps/api/geotags/MapsActivity.kt
+++ GeoTags/app/src/main/java/android/google/maps/api/geotags/MapsActivity.kt
@@ -4,48 +4,43 @@ import android.app.Activity
 import android.content.Intent
 import android.content.IntentSender
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import android.google.maps.api.geotags.databinding.ActivityMapsBinding
 import android.graphics.BitmapFactory
 import android.location.Address
 import android.location.Geocoder
 import android.location.Location
+import android.os.Bundle
 import android.util.Log
 import android.view.*
-import android.widget.Button
 import android.widget.Toast
-import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
+import com.google.android.gms.location.Priority.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import java.io.IOException
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
     GoogleMap.OnMarkerClickListener {
 
     private lateinit var map: GoogleMap
-//    private lateinit var mMap: GoogleMap
 
+    private  lateinit var  searchView:SearchView
 
-
-//////////////////////////////////////////////////////////
     private lateinit var binding: ActivityMapsBinding
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
-    private lateinit var lastLocation: Location
+    private lateinit var lastLocation: Location    //check
 
     private lateinit var locationCallback: LocationCallback
 
@@ -58,19 +53,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         private const val REQUEST_CHECK_SETTINGS = 2
     }
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-//        setSupportActionBar(toolbar)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        setContentView(R.layout.nav_drawer_layout)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
@@ -83,6 +73,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
 
                 lastLocation = p0.lastLocation!!
                 placeMarkerOnMap(LatLng(lastLocation.latitude, lastLocation.longitude))
+                println(getAddress(LatLng(lastLocation.latitude, lastLocation.longitude)))
+                Log.d("address","addres"+getAddress(LatLng(lastLocation.latitude, lastLocation.longitude)))
             }
         }
 
@@ -100,6 +92,38 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.nav_drawer_menu, menu)
+        searchView = findViewById(R.id.idSearchView) //init a searchview
+        val geocoder = Geocoder(this)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                val location: String = searchView.query.toString()
+                val addresses: List<Address>
+                val address:Address
+
+                if(location!=null || location == " "){
+                    try {
+                        addresses = geocoder.getFromLocationName(location,1)
+                        address = addresses[0]
+                        val latLng = LatLng(address.latitude, address.longitude)
+
+                        map.addMarker(MarkerOptions().position(latLng).title(location))
+                        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+
+                    }catch (e:IOException){
+                        Log.d("exception","reason"+e.localizedMessage)
+                        e.localizedMessage
+                    }
+
+
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+
         return true
     }
 
@@ -107,6 +131,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         return when (item.itemId) {
             R.id.addMarkerBtn -> {
                 Log.d("KKK", "I am button")
+                locationCallback = object : LocationCallback() {
+                    override fun onLocationResult(p0: LocationResult) {
+                        super.onLocationResult(p0)
+
+                        lastLocation = p0.lastLocation!!
+                        placeMarkerOnMap(LatLng(lastLocation.latitude, lastLocation.longitude))
+                     Log.d("abc","LatLng(lastLocation.latitude, lastLocation.longitude)="+LatLng(lastLocation.latitude, lastLocation.longitude))
+                    }
+
+                }
                 Toast.makeText(applicationContext, "click on setting", Toast.LENGTH_LONG).show()
                 true
             }
@@ -115,42 +149,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
     }
 
 
-override fun onMapReady(googleMap: GoogleMap) {
-
-
+override fun onMapReady(googleMap: GoogleMap) {   //check
         map = googleMap
 
         map.uiSettings.isZoomControlsEnabled = true
         map.setOnMarkerClickListener(this)
 
         setUpMap()
-
-//        mMap = googleMap
-//
-//        mMap.getUiSettings().setZoomControlsEnabled(true)
-//        mMap.setOnMarkerClickListener(this)
-//
-//        // Add a marker in to coord and move the camera
-//        val coord = LatLng(40.73, -73.99)
-//        val title = "Marker in New York"
-//
-//        mMap.addMarker(MarkerOptions().position(coord).title(title))
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(coord))
-//
-//        //Zooms the camera more to the coord
-//        // zoom = 0 -> zoom all the way out
-//        // zoom = 20 -> max for developed places
-//        // zoom = 13 -> max for remote areas
-//
-//        val zoom = 20.0f
-//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coord, zoom))
-
-
     }
 
     override fun onMarkerClick(p0: Marker): Boolean = false
 
-    private fun setUpMap() {
+    private fun setUpMap() { //check
         if (ActivityCompat.checkSelfPermission(this,
                 android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
@@ -158,14 +168,7 @@ override fun onMapReady(googleMap: GoogleMap) {
             return
         }
 
-
-//        map.mapType = GoogleMap.MAP_TYPE_TERRAIN
-
-//        map.mapType = GoogleMap.MAP_TYPE_NORMAL
-
         map.mapType = GoogleMap.MAP_TYPE_SATELLITE
-
-//        map.mapType = GoogleMap.MAP_TYPE_HYBRID
 
         map.isMyLocationEnabled = true
 
@@ -174,6 +177,7 @@ override fun onMapReady(googleMap: GoogleMap) {
             if (location != null) {
                 lastLocation = location
                 val currentLatLng = LatLng(location.latitude, location.longitude)
+                Log.d("Last loc","Lat"+location.latitude+"Long"+location.longitude)
                 placeMarkerOnMap(currentLatLng)
                 map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
             }
@@ -183,41 +187,16 @@ override fun onMapReady(googleMap: GoogleMap) {
     }
 
 
-//    private fun setUpMap() {
-//        if (ActivityCompat.checkSelfPermission(this,
-//                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions(this,
-//                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
-//            return
-//        }
-//
-//        map.isMyLocationEnabled = true
-//
-//
-//        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
-//            // Got last known location. In some rare situations this can be null.
-//
-//            if (location != null) {
-//                lastLocation = location
-//                val currentLatLng = LatLng(location.latitude, location.longitude)
-//                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
-//            }
-//        }
-//
-//    }
-
     private fun placeMarkerOnMap(location: LatLng) {
 
-        val markerOptions = MarkerOptions().position(location)
+        val markerOptions = MarkerOptions().position(location).draggable(true)
 
         markerOptions.icon(
             BitmapDescriptorFactory.fromBitmap(
                 BitmapFactory.decodeResource(resources, R.mipmap.ic_user_location)))
 
-//        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
-
-
         map.addMarker(markerOptions)
+
     }
 
     private fun startLocationUpdates() {
@@ -233,14 +212,14 @@ override fun onMapReady(googleMap: GoogleMap) {
         fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
     }
 
+    @Suppress("DEPRECATION")
     private fun createLocationRequest() {
 
-        locationRequest = LocationRequest()
-
-        locationRequest.interval = 10000
-
-        locationRequest.fastestInterval = 5000
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest = LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = PRIORITY_HIGH_ACCURACY
+        }
 
         val builder = LocationSettingsRequest.Builder()
             .addLocationRequest(locationRequest)
@@ -271,6 +250,7 @@ override fun onMapReady(googleMap: GoogleMap) {
         }
     }
 
+    @Suppress("DEPRECATION")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
